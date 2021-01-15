277a540632541b378d45d91b4be1dff125d3f2ae
Juraj Kusnier <jurajkusnier@gmail.com>
2021-01-15 15:42:42 +0100

    Add primary label to statistics cards

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/formatter/FormatterStatistics.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/formatter/FormatterStatistics.kt
@@ -2,8 +2,12 @@
 
 package de.rki.coronawarnapp.util.formatter
 
+import android.content.Context
+import de.rki.coronawarnapp.contactdiary.util.getLocale
+import de.rki.coronawarnapp.contactdiary.util.toFormattedDay
 import de.rki.coronawarnapp.server.protocols.internal.stats.KeyFigureCardOuterClass
 import org.joda.time.Instant
+import org.joda.time.LocalDate
 
 fun getPrimaryData(items: List<KeyFigureCardOuterClass.KeyFigure>?): String {
     return items?.firstOrNull { it.rank == KeyFigureCardOuterClass.KeyFigure.Rank.PRIMARY }?.value.toString()
@@ -17,8 +21,20 @@ fun getTertiaryData(items: List<KeyFigureCardOuterClass.KeyFigure>?): String {
     return items?.firstOrNull { it.rank == KeyFigureCardOuterClass.KeyFigure.Rank.TERTIARY }?.value.toString()
 }
 
-fun getPrimaryLabel(instant: Instant?): String {
-    return "Gestern" //TODO: implement me
+fun getPrimaryLabel(instant: Instant?, context: Context): String {
+    val today = LocalDate()
+    val yesterday = today.minusDays(1)
+    return when (val localDate = LocalDate(instant)) {
+        today -> {
+            "Today"
+        }
+        yesterday -> {
+            "Yesterday"
+        }
+        else -> {
+            localDate.toFormattedDay(context.getLocale())
+        }
+    }
 }
 
 
